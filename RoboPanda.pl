9dd22360777f0ce27e797e151d99f6d347da3edb
bcarroll <brettwcarroll@hotmail.com>
2013-08-04 11:54:31 -0400

    Update RoboPanda.pl
    changed tabs to spaces

--- RoboPanda.pl
+++ RoboPanda.pl
@@ -5,9 +5,9 @@ use WowWee::RoboPanda;
 my $COMPORT = 'COM12';
 
 my $robopanda = WowWee::RoboPanda->new(
-		-debug 				=> 1,
-		-serial_port	=> 'COM12',
-	);
+    -debug          => 1,
+    -serial_port    => 'COM12',
+  );
 
 #$robopanda->headlr(1); #move headlr to 1% (far left)
 #$robopanda->headlr(100); #move headlr to 100% (far right)
